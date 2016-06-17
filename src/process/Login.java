e85a40f322bdff4aaa3b25f9fc5f67bb466ce419
mauriciounipampa <mauriciosescobar@gmail.com>
2016-06-17 20:06:15 -0300

    Cadastrar Médico

--- src/process/Login.java
+++ src/process/Login.java
@@ -9,12 +9,10 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
 import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
-import javax.swing.plaf.OptionPaneUI;
 
 /**
  *
